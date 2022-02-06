f91ef3cb1cc2de5cabfb27d6a078837cdc927e79
Aleksandr "Flone" Tretyakov <flonednb@gmail.com>
2022-02-06 20:51:57 +0500

    Add buttons to change pages.

--- client/src/layouts/main_layout.rs
+++ client/src/layouts/main_layout.rs
@@ -5,19 +5,19 @@ use druid::WidgetExt;
 
 // Custom.
 use crate::widgets::report::ReportWidget;
-use crate::{ApplicationState, Layout};
+use crate::ApplicationState;
 
 // Layout customization.
-const TEXT_SIZE: f64 = 16.0;
+const TEXT_SIZE: f64 = 18.0;
 
 #[derive(Clone, Data)]
 pub struct MainLayout {
-    pub refresh_ui: bool,
+    pub current_page: u64,
 }
 
 impl MainLayout {
     pub fn new() -> Self {
-        Self { refresh_ui: false }
+        Self { current_page: 1 }
     }
     pub fn build_ui() -> impl Widget<ApplicationState> {
         let report = ReportWidget::new(
@@ -39,8 +39,63 @@ impl MainLayout {
                         .with_child(report.build_ui())
                         .with_child(report.build_ui())
                         .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
+                        .with_child(report.build_ui())
                         .with_child(report.build_ui()),
                     1.0,
+                )
+                .with_default_spacer()
+                .with_child(
+                    Flex::row()
+                        .must_fill_main_axis(true)
+                        .main_axis_alignment(MainAxisAlignment::Center)
+                        .with_flex_child(
+                            Button::from_label(
+                                Label::new("Show First Page").with_text_size(TEXT_SIZE),
+                            )
+                            .disabled_if(|data: &ApplicationState, _env| {
+                                data.main_layout.current_page == 1
+                            })
+                            .align_left(),
+                            0.25,
+                        )
+                        .with_flex_child(
+                            Flex::column()
+                                .with_child(Label::new("page").with_text_size(TEXT_SIZE))
+                                .with_child(
+                                    Flex::row()
+                                        .with_child(
+                                            Button::from_label(
+                                                Label::new("<").with_text_size(TEXT_SIZE),
+                                            )
+                                            .disabled_if(|data: &ApplicationState, _env| {
+                                                data.main_layout.current_page == 1
+                                            }),
+                                        )
+                                        .with_child(Label::new("P").with_text_size(TEXT_SIZE))
+                                        .with_child(Button::from_label(
+                                            Label::new(">").with_text_size(TEXT_SIZE),
+                                        )),
+                                )
+                                .with_child(Label::new("out of N").with_text_size(TEXT_SIZE)),
+                            0.5,
+                        )
+                        .with_flex_child(
+                            Button::from_label(
+                                Label::new("Show Last Page").with_text_size(TEXT_SIZE),
+                            )
+                            .align_right(),
+                            0.25,
+                        ),
                 ),
         )
     }

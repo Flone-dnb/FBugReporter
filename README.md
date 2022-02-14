d3b82eba5494a668911fccb355b5c91c3506c921
Aleksandr "Flone" Tretyakov <flonednb@gmail.com>
2022-02-14 23:22:06 +0500

    Generate theme.ini file at startup if one does not exist or if some values are missing.

--- README.md
+++ README.md
@@ -11,18 +11,18 @@ The server processes reporters and clients on different ports (see <b>server_con
 
 # Client
 
-Theme can be customized by copy-pasting the <b>theme.ini</b> file from <b>client</b> folder next to the client executable file.<br>
-If there is a file <b>theme.ini</b> next to the client binary it will read theme values from it instead of using default values.
+On first start, the client will create a theme file <b>theme.ini</b> next to the binary file.<br>
+You can customize values in this theme file. In order for them to be applied, restart the client.
 
 # Build
 
 <h2>Reporter</h2>
-<b><u>In order to build</u></b> the reporter you will need Rust and LLVM installed, then in the /reporter folder run:
+<h4>To build</h4> the reporter you will need Rust and LLVM installed, then in the /reporter folder run:
 <pre>
 cargo build --release
 </pre>
 Compiled library will be located at /reporter/target/release/ (with the extension ".dll" for Windows and ".so" for Linux)<br>
 <br>
-<b><u>In order to integrate</u></b> the reporter you will need to create a GDNativeLibrary resource in your Godot project, you can call it "reporter". Then navigate to your platform in the opened panel. Click on the folder icon (against number "64" for 64 bit systems, "32" for 32 bit systems) and select the compiled library. Save this resource with the ".gdnlib" extension.<br>
+<h4>To integrate</h4> the reporter you will need to create a GDNativeLibrary resource in your Godot project, you can call it "reporter". Then navigate to your platform in the opened panel. Click on the folder icon (against number "64" for 64 bit systems, "32" for 32 bit systems) and select the compiled library. Save this resource with the ".gdnlib" extension.<br>
 Now create a new script with the following parameters: "Language" to "NativeScript", "Inherits" to "Node", "Class Name" to "Reporter" and change the name of the script (file) in the "Path" to "reporter". Then open this script and in the "Inspector" panel find property with the name "Library", click on it, then pick "Load" and select the reporter.gdnlib (GDNativeLibrary) file the we created.<br><br>
 See the example project in the <b>example</b> folder and <b>example/MainScene.gd</b> for how to send reports. You could just copy-paste <b>MainScene.tscn</b> and <b>MainScene.gd</b> files to your project and customize them as you want.
--- client/src/theme.rs
+++ client/src/theme.rs
@@ -3,7 +3,20 @@ use configparser::ini::Ini;
 use druid::widget::prelude::*;
 use druid::{Color, Lens};
 
+// Custom.
+use crate::misc::app_error::AppError;
+
+const CONFIG_THEME_FILE_NAME: &str = "theme.ini";
 const CONFIG_THEME_SECTION_NAME: &str = "theme";
+const CONFIG_THEME_BACKGROUND_COLOR_PARAM: &str = "background_color";
+const CONFIG_THEME_PLACEHOLDER_COLOR_PARAM: &str = "placeholder_color";
+const CONFIG_THEME_TEXTBOX_BACKGROUND_COLOR_PARAM: &str = "textbox_background_color";
+const CONFIG_THEME_TEXT_SELECTION_COLOR_PARAM: &str = "text_selection_color";
+const CONFIG_THEME_ACTIVE_BORDER_COLOR_PARAM: &str = "active_border_color";
+const CONFIG_THEME_INACTIVE_BORDER_COLOR_PARAM: &str = "inactive_border_color";
+const CONFIG_THEME_BUTTON_DARK_COLOR_PARAM: &str = "button_dark_color";
+const CONFIG_THEME_BUTTON_LIGHT_COLOR_PARAM: &str = "button_light_color";
+const CONFIG_THEME_BORDER_RADIUS_PARAM: &str = "border_radius";
 
 #[derive(Clone, Data, Lens, Debug)]
 pub struct ApplicationTheme {
@@ -20,44 +33,160 @@ pub struct ApplicationTheme {
 
 impl ApplicationTheme {
     pub fn new() -> Self {
+        let mut theme = ApplicationTheme::default();
+
         // Try reading theme from .ini file.
         let mut config = Ini::new();
         let map = config.load("theme.ini");
-        if let Err(e) = map {
-            println!("{:?}\nUsing default theme.", e);
-
-            return Self::default();
+        if map.is_err() {
+            println!(
+                "INFO: could not open the theme file \"{0}\", using default values \
+                and creating a new \"{0}\" theme file.",
+                CONFIG_THEME_FILE_NAME
+            );
+            // No file found, create a new file.
+            if let Err(e) = theme.save_theme() {
+                // Non-critical error.
+                print!(
+                    "WARNING: {}",
+                    AppError::new(&e.to_string(), file!(), line!())
+                );
+            }
+            return theme;
         }
 
-        let mut theme = Self::default();
-
-        read_theme_color_hex("background_color", &mut theme.background_color, &config);
-        read_theme_color_hex("placeholder_color", &mut theme.placeholder_color, &config);
-        read_theme_color_hex(
-            "textbox_background_color",
+        let mut some_values_were_empty = false;
+        if read_theme_color_hex(
+            CONFIG_THEME_BACKGROUND_COLOR_PARAM,
+            &mut theme.background_color,
+            &config,
+        ) {
+            some_values_were_empty = true;
+        }
+        if read_theme_color_hex(
+            CONFIG_THEME_PLACEHOLDER_COLOR_PARAM,
+            &mut theme.placeholder_color,
+            &config,
+        ) {
+            some_values_were_empty = true;
+        }
+        if read_theme_color_hex(
+            CONFIG_THEME_TEXTBOX_BACKGROUND_COLOR_PARAM,
             &mut theme.textbox_background_color,
             &config,
-        );
-        read_theme_color_hex(
-            "inactive_border_color",
+        ) {
+            some_values_were_empty = true;
+        }
+        if read_theme_color_hex(
+            CONFIG_THEME_INACTIVE_BORDER_COLOR_PARAM,
             &mut theme.inactive_border_color,
             &config,
-        );
-        read_theme_color_hex(
-            "active_border_color",
+        ) {
+            some_values_were_empty = true;
+        }
+        if read_theme_color_hex(
+            CONFIG_THEME_ACTIVE_BORDER_COLOR_PARAM,
             &mut theme.active_border_color,
             &config,
-        );
-        read_theme_color_hex(
-            "text_selection_color",
+        ) {
+            some_values_were_empty = true;
+        }
+        if read_theme_color_hex(
+            CONFIG_THEME_TEXT_SELECTION_COLOR_PARAM,
             &mut theme.text_selection_color,
             &config,
-        );
-        read_theme_color_hex("button_dark_color", &mut theme.button_dark_color, &config);
-        read_theme_color_hex("button_light_color", &mut theme.button_light_color, &config);
-        read_theme_param_float("border_radius", &mut theme.border_radius, &config);
+        ) {
+            some_values_were_empty = true;
+        }
+        if read_theme_color_hex(
+            CONFIG_THEME_BUTTON_DARK_COLOR_PARAM,
+            &mut theme.button_dark_color,
+            &config,
+        ) {
+            some_values_were_empty = true;
+        }
+        if read_theme_color_hex(
+            CONFIG_THEME_BUTTON_LIGHT_COLOR_PARAM,
+            &mut theme.button_light_color,
+            &config,
+        ) {
+            some_values_were_empty = true;
+        }
+        if read_theme_param_float(
+            CONFIG_THEME_BORDER_RADIUS_PARAM,
+            &mut theme.border_radius,
+            &config,
+        ) {
+            some_values_were_empty = true;
+        }
+
+        if some_values_were_empty {
+            // Create a new file with all values filled.
+            if let Err(e) = theme.save_theme() {
+                // Non-critical error.
+                print!(
+                    "WARNING: {}",
+                    AppError::new(&e.to_string(), file!(), line!())
+                );
+            }
+        }
 
         theme
+    }
+    fn save_theme(&self) -> Result<(), AppError> {
+        let mut config = Ini::new();
+
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_BACKGROUND_COLOR_PARAM,
+            Some(&format!("{:?}", self.background_color)[1..]),
+        );
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_PLACEHOLDER_COLOR_PARAM,
+            Some(&format!("{:?}", self.placeholder_color)[1..]),
+        );
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_TEXTBOX_BACKGROUND_COLOR_PARAM,
+            Some(&format!("{:?}", self.textbox_background_color)[1..]),
+        );
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_INACTIVE_BORDER_COLOR_PARAM,
+            Some(&format!("{:?}", self.inactive_border_color)[1..]),
+        );
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_ACTIVE_BORDER_COLOR_PARAM,
+            Some(&format!("{:?}", self.active_border_color)[1..]),
+        );
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_TEXT_SELECTION_COLOR_PARAM,
+            Some(&format!("{:?}", self.text_selection_color)[1..]),
+        );
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_BUTTON_DARK_COLOR_PARAM,
+            Some(&format!("{:?}", self.button_dark_color)[1..]),
+        );
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_BUTTON_LIGHT_COLOR_PARAM,
+            Some(&format!("{:?}", self.button_light_color)[1..]),
+        );
+        config.setstr(
+            CONFIG_THEME_SECTION_NAME,
+            CONFIG_THEME_BORDER_RADIUS_PARAM,
+            Some(&self.border_radius.to_string()),
+        );
+
+        if let Err(e) = config.write(CONFIG_THEME_FILE_NAME) {
+            return Err(AppError::new(&e.to_string(), file!(), line!()));
+        }
+
+        Ok(())
     }
 }
 
@@ -67,7 +196,7 @@ impl Default for ApplicationTheme {
             background_color: Color::rgb8(30, 26, 22),
             placeholder_color: Color::rgb8(65, 60, 55),
             textbox_background_color: Color::rgb8(35, 30, 25),
-            inactive_border_color: Color::rgba(0.0, 0.0, 0.0, 0.0),
+            inactive_border_color: Color::rgb8(181, 98, 2),
             active_border_color: Color::rgb8(181, 98, 2),
             text_selection_color: Color::rgb8(181, 98, 2),
             button_dark_color: Color::rgb8(181, 98, 2),
@@ -77,35 +206,54 @@ impl Default for ApplicationTheme {
     }
 }
 
-fn read_theme_color_hex(param: &str, color: &mut Color, config: &Ini) {
+/// Read color parameter from hex string.
+///
+/// Returns `true` if the value was empty, `false` if it was set.
+fn read_theme_color_hex(param: &str, color: &mut Color, config: &Ini) -> bool {
+    let mut value_was_empty = true;
+
     let value = config.get(CONFIG_THEME_SECTION_NAME, param);
     if value.is_some() {
         let value = value.unwrap();
-        let new_color = Color::from_hex_str(&value);
-        if let Err(ref e) = new_color {
-            println!(
-                "ERROR: could not parse value of theme parameter '{}', error: {}.",
-                param, e
-            );
-        } else {
-            *color = new_color.unwrap();
+
+        if !value.is_empty() {
+            let new_color = Color::from_hex_str(&value);
+            if let Err(ref e) = new_color {
+                println!(
+                    "ERROR: could not parse value of theme parameter '{}', error: {}.",
+                    param, e
+                );
+            } else {
+                *color = new_color.unwrap();
+                value_was_empty = false;
+            }
         }
     }
+
+    return value_was_empty;
 }
 
-fn read_theme_param_float(param: &str, input: &mut f64, config: &Ini) {
+/// Read float parameter from string.
+///
+/// Returns `true` if the value was empty, `false` if it was set.
+fn read_theme_param_float(param: &str, input: &mut f64, config: &Ini) -> bool {
+    let mut value_was_empty = true;
+
     let value = config.getfloat(CONFIG_THEME_SECTION_NAME, param);
     if let Err(e) = value {
         println!(
             "ERROR: could not parse value of theme parameter '{}', error: {}.",
             param, e
         );
-        return;
-    }
-    let value = value.unwrap();
-    if value.is_some() {
-        *input = value.unwrap();
     } else {
-        println!("ERROR: param '{}' is None.", param);
+        let value = value.unwrap();
+        if value.is_some() {
+            *input = value.unwrap();
+            value_was_empty = false;
+        } else {
+            println!("ERROR: param '{}' is None.", param);
+        }
     }
+
+    return value_was_empty;
 }

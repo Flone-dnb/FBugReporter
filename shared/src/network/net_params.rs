5c40d3be30b3a58790bfaf087889be37952062c3
Aleksandr Tretyakov <flonednb@gmail.com>
2022-07-22 19:15:41 +0500

    Increase wait time in I/O.

--- shared/src/network/net_params.rs
+++ shared/src/network/net_params.rs
@@ -3,7 +3,7 @@ pub const A_B_BITS: u64 = 2048; // if changed, change protocol version
 pub const IV_LENGTH: usize = 16; // if changed, change protocol version
 pub const CMAC_TAG_LENGTH: usize = 16; // if changed, change protocol version
 
-pub const MAX_WAIT_TIME_IN_READ_WRITE_MS: u64 = 5000; // 5 minutes
+pub const MAX_WAIT_TIME_IN_READ_WRITE_MS: u64 = 120000; // 2 minutes
 pub const WOULD_BLOCK_RETRY_AFTER_MS: u64 = 20;
 
 pub const NETWORK_PROTOCOL_VERSION: u16 = 1;

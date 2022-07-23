a0370c0c072fa7b6ae1da04910240d9b3d63dd7a
Aleksandr Tretyakov <flonednb@gmail.com>
2022-07-23 13:49:12 +0500

    Bump version to v1.1.0.

--- shared/src/network/net_params.rs
+++ shared/src/network/net_params.rs
@@ -6,4 +6,4 @@ pub const CMAC_TAG_LENGTH: usize = 16; // if changed, change protocol version
 pub const MAX_WAIT_TIME_IN_READ_WRITE_MS: u64 = 120000; // 2 minutes
 pub const WOULD_BLOCK_RETRY_AFTER_MS: u64 = 20;
 
-pub const NETWORK_PROTOCOL_VERSION: u16 = 1;
+pub const NETWORK_PROTOCOL_VERSION: u16 = 2;

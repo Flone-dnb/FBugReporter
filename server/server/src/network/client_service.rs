Flone-dnb/FBugReporter

commit 6d9c10e11baafc649e894331269a4ccc9d26f486

Update dependencies, fix build, support Godot 4.

server/server/src/network/client_service.rs
@@ -181,12 +181,9 @@ impl ClientService {
                     password,
                     otp,
                     None,
-                );
-                if let Err(app_error) = result {
-                    return Err(app_error);
-                }
+                )?;
 
-                Ok(result.unwrap())
+                Ok(result)
             }
             ClientRequest::SetFirstPassword {
                 client_net_protocol,
@@ -200,19 +197,12 @@ impl ClientService {
                     old_password,
                     String::new(),
                     Some(new_password),
-                );
-                if let Err(app_error) = result {
-                    return Err(app_error);
-                }
+                )?;
 
-                Ok(result.unwrap())
+                Ok(result)
             }
             ClientRequest::QueryReportsSummary { page, amount } => {
-                let result = self.handle_client_reports_request(page, amount);
-                if let Err(app_error) = result {
-                    return Err(app_error);
-                }
-
+                self.handle_client_reports_request(page, amount)?;
                 Ok(None)
             }
             ClientRequest::QueryReport { report_id } => {
@@ -232,11 +222,7 @@ impl ClientService {
                 Ok(None)
             }
             ClientRequest::DeleteReport { report_id } => {
-                let result = self.handle_client_delete_report_request(report_id);
-                if let Err(app_error) = result {
-                    return Err(app_error);
-                }
-
+                self.handle_client_delete_report_request(report_id)?;
                 Ok(None)
             }
         }
@@ -282,23 +268,15 @@ impl ClientService {
 
         // Get user's password and salt.
         let database_guard = self.database.lock().unwrap();
-        let result = database_guard.get_user_password_and_salt(&username);
+        let (db_password, salt) = database_guard.get_user_password_and_salt(&username)?;
         drop(database_guard);
 
-        if let Err(app_error) = result {
-            return Err(app_error);
-        }
-
         // Check if user exists.
-        let (db_password, salt) = result.unwrap();
         if db_password.is_empty() {
             // No user was found for this username.
-            let result = self.answer_client_wrong_credentials(&username);
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
+            let result = self.answer_client_wrong_credentials(&username)?;
 
-            return Ok(Some(result.unwrap()));
+            return Ok(Some(result));
         }
 
         // Compare passwords.
@@ -312,26 +290,19 @@ impl ClientService {
 
         if password_hash != db_password {
             // Wrong password.
-            let result = self.answer_client_wrong_credentials(&username);
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
+            let result = self.answer_client_wrong_credentials(&username)?;
 
-            return Ok(Some(result.unwrap()));
+            return Ok(Some(result));
         }
 
         // See if user needs to set first password.
         let mut _need_change_password = false;
         {
-            let result = self
+            _need_change_password = self
                 .database
                 .lock()
                 .unwrap()
-                .is_user_needs_to_change_password(&username);
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
-            _need_change_password = result.unwrap();
+                .is_user_needs_to_change_password(&username)?;
         }
 
         if _need_change_password && new_password.is_none() {
@@ -362,11 +333,7 @@ impl ClientService {
                 .database
                 .lock()
                 .unwrap()
-                .update_user_password(&username, new_password);
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
-            let result = result.unwrap();
+                .update_user_password(&username, new_password)?;
             if result {
                 return Ok(Some(format!(
                     "received new password from user {}, but \
@@ -384,18 +351,10 @@ impl ClientService {
         // Check if user needs to setup OTP (receive OTP QR code).
         {
             let db_guard = self.database.lock().unwrap();
-            let result = db_guard.is_user_needs_setup_otp(&username);
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
-            let _need_setup_otp = result.unwrap();
+            let _need_setup_otp = db_guard.is_user_needs_setup_otp(&username)?;
 
             // Get OTP secret.
-            let result = db_guard.get_otp_secret_key_for_user(&username);
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
-            let otp_secret = result.unwrap();
+            let otp_secret = db_guard.get_otp_secret_key_for_user(&username)?;
 
             drop(db_guard);
 
@@ -479,21 +438,14 @@ impl ClientService {
                         LogCategory::Info,
                         &format!("{} tried to login using wrong OTP code.", &username),
                     );
-                    let result = self.answer_client_wrong_credentials(&username);
-                    if let Err(app_error) = result {
-                        return Err(app_error);
-                    }
+                    let result = self.answer_client_wrong_credentials(&username)?;
 
-                    return Ok(Some(result.unwrap()));
+                    return Ok(Some(result));
                 } else if _need_setup_otp {
-                    let result = self
-                        .database
+                    self.database
                         .lock()
                         .unwrap()
-                        .set_user_finished_otp_setup(&username);
-                    if let Err(app_error) = result {
-                        return Err(app_error);
-                    }
+                        .set_user_finished_otp_setup(&username)?;
                     self.logger.lock().unwrap().print_and_log(
                         LogCategory::Info,
                         &format!("{} finished OTP setup.", &username),
@@ -507,19 +459,13 @@ impl ClientService {
             let guard = self.database.lock().unwrap();
 
             // Update last login time/date/ip.
-            if let Err(app_error) = guard.update_user_last_login(
+            guard.update_user_last_login(
                 &username,
                 &self.socket.peer_addr().unwrap().ip().to_string(),
-            ) {
-                return Err(app_error);
-            }
+            )?;
 
             // Check if user is admin.
-            let result = guard.is_user_admin(&username);
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
-            _is_admin = result.unwrap();
+            _is_admin = guard.is_user_admin(&username)?;
         }
 
         {
@@ -561,21 +507,9 @@ impl ClientService {
     fn handle_client_reports_request(&mut self, page: u64, amount: u64) -> Result<(), AppError> {
         // Get reports from database.
         let guard = self.database.lock().unwrap();
-        let result = guard.get_reports(page, amount);
-        let report_count = guard.get_report_count();
+        let reports = guard.get_reports(page, amount)?;
+        let report_count = guard.get_report_count()?;
         drop(guard);
-
-        // Check reports.
-        if let Err(app_error) = result {
-            return Err(app_error);
-        }
-        let reports = result.unwrap();
-
-        // Check report count.
-        if let Err(app_error) = report_count {
-            return Err(app_error);
-        }
-        let report_count = report_count.unwrap();
 
         // Get disk space.
         let (mut _total_disk_space_mb, mut _used_disk_space_mb) = (0u64, 0u64);
@@ -641,11 +575,7 @@ impl ClientService {
         }
 
         // Remove report from database.
-        let result = self.database.lock().unwrap().remove_report(report_id);
-        if let Err(app_error) = result {
-            return Err(app_error);
-        }
-        let found = result.unwrap();
+        let found = self.database.lock().unwrap().remove_report(report_id)?;
         if !found {
             let mut username = String::new();
             if self.username.is_some() {
@@ -868,20 +798,16 @@ impl ClientService {
         self.time_of_last_received_message = Local::now();
 
         loop {
-            let result = receive_message(
+            let message = receive_message(
                 &mut self.socket,
                 &self.secret_key,
                 Some(KEEP_ALIVE_CHECK_INTERVAL_MS),
                 MAX_MESSAGE_SIZE_IN_BYTES_WITHOUT_ATTACHMENTS,
                 &mut is_fin,
-            );
+            )?;
             if is_fin {
                 return Ok(None);
             }
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
-            let message = result.unwrap();
 
             if message.is_empty() {
                 // Timeout.
@@ -902,11 +828,7 @@ impl ClientService {
             let message = message.unwrap();
 
             // Handle message.
-            let result = self.handle_client_message(message);
-            if let Err(app_error) = result {
-                return Err(app_error);
-            }
-            let result = result.unwrap();
+            let result = self.handle_client_message(message)?;
             if result.is_some() {
                 return Ok(result);
             }

LICENSE
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2021-2022 Aleksandr "Flone" Tretyakov
+Copyright (c) 2021-2023 Aleksandr "Flone" Tretyakov
 
 Permission is hereby granted, free of charge, to any
 person obtaining a copy of this software and associated

client/Cargo.toml
@@ -1,21 +1,21 @@
 [package]
 name = "client"
 authors = ["Aleksandr Tretyakov"]
-version = "1.1.0"
+version = "1.2.0"
 edition = "2021"
 license = "MIT"
 
 [dependencies]
-druid = { git = "https://github.com/linebender/druid.git" }
-rdev = "0.5.1"
-configparser = "3.0.0"
-opener = "0.5.0"
-serde = {version = "1.0.137", features = ["derive"]}
+druid = "0.8.3"
+rdev = "0.5.2"
+configparser = "3.0.2"
+opener = "0.6.1"
+serde = {version = "1.0.163", features = ["derive"]}
 bincode = "1.3.3"
-chrono = "0.4.22"
-sha2 = "0.10.2"
-image = "0.24.2"
-base64 = "0.13.0"
+chrono = "0.4.24"
+sha2 = "0.10.6"
+image = "0.24.6"
+base64 = "0.21.0"
 native-dialog = {version = "0.6.3", features = ["windows_dpi_awareness", "windows_visual_styles"]}
 shared = {path = "../shared"}
 

client/src/layouts/otp_layout.rs
@@ -1,4 +1,5 @@
 // External.
+use base64::{engine::general_purpose, Engine as _};
 use druid::widget::{prelude::*, LineBreaking, SizedBox};
 use druid::widget::{Button, Flex, Label, MainAxisAlignment, TextBox};
 use druid::{
@@ -36,7 +37,9 @@ impl OtpLayout {
         let mut qr_code_item = Flex::column();
         if self.qr_code.is_some() {
             // Decode base64.
-            let decoded = base64::decode(self.qr_code.as_ref().unwrap()).unwrap();
+            let decoded = general_purpose::STANDARD
+                .decode(self.qr_code.as_ref().unwrap())
+                .unwrap();
 
             // Convert to image pixels.
             let mut img = image::load_from_memory(decoded.as_slice()).unwrap();

client/src/main.rs
@@ -109,7 +109,7 @@ pub fn main() {
 fn apply_theme(env: &mut Env, data: &ApplicationState) {
     env.set(
         druid::theme::WINDOW_BACKGROUND_COLOR,
-        data.theme.background_color.clone(),
+        data.theme.background_color,
     );
     env.set(
         druid::theme::TEXTBOX_BORDER_RADIUS,
@@ -118,32 +118,20 @@ fn apply_theme(env: &mut Env, data: &ApplicationState) {
     env.set(druid::theme::BUTTON_BORDER_RADIUS, data.theme.border_radius);
     env.set(
         druid::theme::PLACEHOLDER_COLOR,
-        data.theme.placeholder_color.clone(),
+        data.theme.placeholder_color,
     );
     env.set(
         druid::theme::BACKGROUND_LIGHT,
-        data.theme.textbox_background_color.clone(),
+        data.theme.textbox_background_color,
     );
-    env.set(
-        druid::theme::BORDER_DARK,
-        data.theme.inactive_border_color.clone(),
-    );
+    env.set(druid::theme::BORDER_DARK, data.theme.inactive_border_color);
     env.set(
         druid::theme::SELECTED_TEXT_BACKGROUND_COLOR,
-        data.theme.text_selection_color.clone(),
+        data.theme.text_selection_color,
     );
-    env.set(
-        druid::theme::PRIMARY_LIGHT,
-        data.theme.active_border_color.clone(),
-    );
-    env.set(
-        druid::theme::BUTTON_DARK,
-        data.theme.button_dark_color.clone(),
-    );
-    env.set(
-        druid::theme::BUTTON_LIGHT,
-        data.theme.button_light_color.clone(),
-    );
+    env.set(druid::theme::PRIMARY_LIGHT, data.theme.active_border_color);
+    env.set(druid::theme::BUTTON_DARK, data.theme.button_dark_color);
+    env.set(druid::theme::BUTTON_LIGHT, data.theme.button_light_color);
 }
 
 fn build_root_widget() -> impl Widget<ApplicationState> {

client/src/network/net_service.rs
@@ -332,20 +332,16 @@ impl NetService {
 
         // Wait for answer.
         let mut is_fin = false;
-        let result = receive_message(
+        let serialized_packet = receive_message(
             self.socket.as_mut().unwrap(),
             &self.secret_key,
             None,
             std::usize::MAX,
             &mut is_fin,
-        );
+        )?;
         if is_fin {
             return Err(AppError::new("unexpected FIN received"));
         }
-        if let Err(app_error) = result {
-            return Err(app_error);
-        }
-        let serialized_packet = result.unwrap();
 
         // Deserialize.
         let message = bincode::deserialize::<ClientAnswer>(&serialized_packet);
@@ -385,20 +381,16 @@ impl NetService {
 
         // Wait for answer.
         let mut is_fin = false;
-        let result = receive_message(
+        let serialized_packet = receive_message(
             self.socket.as_mut().unwrap(),
             &self.secret_key,
             None,
             std::usize::MAX,
             &mut is_fin,
-        );
+        )?;
         if is_fin {
             return Err(AppError::new("unexpected FIN received"));
         }
-        if let Err(app_error) = result {
-            return Err(app_error);
-        }
-        let serialized_packet = result.unwrap();
 
         // Deserialize.
         let message = bincode::deserialize::<ClientAnswer>(&serialized_packet);

example/.gitattributes
@@ -1,0 +1,2 @@
+# Normalize EOL for all files that Git considers text files.
+* text=auto eol=lf

example/.gitignore
@@ -1,2 +1,4 @@
-reporter.log
-bin/*
+# Godot 4+ specific ignores
+.godot/*
+
+!.godot/extension_list.cfg

commit 638e75cec9ca3b63e70328cda54d3b11a7dbd507

Fix a bug introduced in `6d9c10e` that causes a misleading log.
It caused the server to think that there was an unexpected
FIN received when the client was closing the connection while
in reality there was no error.

server/server/src/network/client_service.rs
@@ -480,10 +480,10 @@ impl ClientService {
 
         {
             // Mark user as logged in.
-            self.logger
-                .lock()
-                .unwrap()
-                .print_and_log(LogCategory::Info, &format!("{} logged in", &username));
+            self.logger.lock().unwrap().print_and_log(
+                LogCategory::Info,
+                &format!("{} ({}) logged in", &username, self.socket_addr),
+            );
         }
 
         self.username = Some(username);

shared/src/network/messaging.rs
@@ -502,6 +502,7 @@ where
 /// - `is_fin`: will be `true` if the remote socket closed connection.
 ///
 /// ## Return
+/// Empty `Ok` array if received FIN from remote connection (connection is being closed).
 /// If custom timeout was specified and reached a timeout will return `Ok` with zero length vector,
 /// otherwise if custom timeout was not specified will return `AppError`.
 pub fn receive_message(
@@ -535,10 +536,7 @@ pub fn receive_message(
     match result {
         IoResult::Fin => {
             *is_fin = true;
-            return Err(AppError::new(&format!(
-                "unexpected FIN received (socket: {})",
-                socket_addr
-            )));
+            return Ok(Vec::new());
         }
         IoResult::Err(app_error) => return Err(app_error),
         IoResult::Ok(byte_count) => {